daf6b4d3549b8c5a09383be0fd937f6179f19c01
Seungbin Oh <sboh1214@gmail.com>
2019-12-04 11:51:16 +0900

    fix error

--- music_player/settings/base.py
+++ music_player/settings/base.py
@@ -109,7 +109,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = ''
+STATIC_ROOT = '/home/sboh1214/repos/music-player/static/'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (join('static'), )
 
